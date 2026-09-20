5131b5898d43270b27fe3c9553667e39771eefb0
KingBean4903 <davidkanyango065@gmail.com>
2026-09-20 13:11:50 +0300

    Added jobrepository

--- src/main/java/com/example/demo/job/JobRepository.java
+++ src/main/java/com/example/demo/job/JobRepository.java
@@ -1,7 +1,12 @@
 package com.example.demo.job;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobRepository extends JpaRepository<Job, UUID> {}
+public interface JobRepository extends JpaRepository<Job, UUID> {
+	
+	List<Job> findTop100ByStatusAndNextRunAtAsc(JobStatus status, Instant now);
+}
